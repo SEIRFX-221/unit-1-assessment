ddf5cd863ddbc6b85aa55d5af76d709362295a8b
rufuscodes <rufuscodes@gmail.com>
2023-04-15 14:14:33 -0700

    Done writting java script for now

--- js/main.js
+++ js/main.js
@@ -1,49 +1,39 @@
-const counterValue = 0;
+let counterValue = 0;
 
- 
+let body = document.querySelector('body');
 
-// Select body tag from boilerplate
-const body = document.querySelector('body');
-console.log(body);
-
-// created html element 'div' to conatin the counter 
-const counterContainer = document.createElement('div');
+let counterContainer = document.createElement('section');
 counterContainer.setAttribute('id', 'counter-container');
-console.log(counterContainer);
 
-// created buttons for the UI
-const addBtn = document.createElement('button');
+let addBtn = document.createElement('button');
 addBtn.textContent = '+';
-const minusBtn = document.createElement('button')
+let minusBtn = document.createElement('button')
 minusBtn.textContent = '-';
-// Test the newly created elements
-console.log(addBtn, minusBtn);
-const digitInput = document.createElement('input');
+let digitInput = document.createElement('input');
 digitInput.placeholder = '0';
-console.log(digitInput);
-const displayText = document.createElement('span');
+let displayText = document.createElement('div');
 displayText.setAttribute('id', 'display-text')
-displayText.textContent = counterValue.valueOf(counterValue);
-// testing the span for displaying digits
-console.log(displayText);
-
+displayText.textContent = counterValue;
 
 body.appendChild(counterContainer);
 counterContainer.appendChild(displayText);
 counterContainer.appendChild(minusBtn);
 counterContainer.appendChild(digitInput);
 counterContainer.appendChild(addBtn);
+digiVal = document.querySelector('input').value;
+console.log(digiVal);
+addBtn.addEventListener("click", function () {
+    digiVal = document.querySelector('input').value;
+    console.log(digiVal);
+    counterValue++;
+    displayText.textContent = counterValue += parseInt(digiVal);
+    console.log(displayText);
+});
 
-console.log(body);
+minusBtn.addEventListener("click", function () {
+    digiVal = document.querySelector('input').value;
+    counterValue--;
+    displayText.textContent = counterValue -= parseInt(digiVal);
+    console.log(digitInput.value);
+});
 
-
-
-
-addBtn.addEventListener("click", function(){
-
-
-  });
-
-  minusBtn.addEventListener("click", function(){
-
-   });
