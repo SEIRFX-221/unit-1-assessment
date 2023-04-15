d22f57c9dd51a061d47539861d61996fbaf81c83
Jonathan Davila <jonathandavila@Jonathans-MacBook-Air.local>
2023-04-15 11:14:35 -0700

    feat: +/- buttons add/subtract value

--- js/main.js
+++ js/main.js
@@ -1,1 +1,24 @@
-console.log('connected')
+// GLOBAL VARIABLES
+let number = 0;
+let numberDisplay = document.querySelector('#number-display');
+let addButton = document.querySelector('#add');
+let subtractButton = document.querySelector('#subtract');
+let numberInput = document.querySelector('#number-input')
+// GLOBAL VARIABLES
+
+
+// EVENT LISTENERS
+window.addEventListener('DOMContentLoaded', () => {
+    numberDisplay.textContent = number;
+})
+
+addButton.addEventListener('click', () => {
+    number += parseInt(numberInput.value)
+    numberDisplay.textContent = number
+} )
+
+subtractButton.addEventListener('click', () => {
+    number -= parseInt(numberInput.value)
+    numberDisplay.textContent = number
+} )
+// EVENT LISTENERS
