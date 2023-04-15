cc3a55738f451225cb255dc5a1869cdae50f2e55
Alexis Laronga <alexislaronga@Alexiss-MacBook-Air.local>
2023-04-15 15:53:00 -0400

    add: Bonus and finishing CSS styling

--- app.js
+++ app.js
@@ -4,8 +4,9 @@ const plus = document.getElementById('plus');
 const input = document.getElementById('input');
 const minus = document.getElementById('minus');
 
-input.value = 1;
-result.textContent = '0';
+
+result.value = 0;
+// result.innerText = Number('0');
 
 function addition(num1, num2) {
     return num1 + num2;
@@ -14,19 +15,38 @@ function subtraction(num1, num2) {
     return num1 - num2;
 }
 
+
+
 plus.addEventListener('click', function () {
-    let inputValue = input.value;
-    let resultValue = result.value;
-    result.textContent = addition(inputValue, resultValue);
+    let inputValue = Number(document.getElementById('input').value);
+    let resultValue = Number(document.getElementById('result').value);
+    result.value = addition(inputValue, resultValue);
     console.log('this shouldnt be undefined', result.value);
-    return result;
+    result.innerText = result.value;
+    if (result.value >= 0) {
+        result.style.color = 'black';
+    } else if (result.value < 0) {
+        result.style.color = 'red';
+    } else {
+        return null;
+    }
+    return resultValue;
 })
 
 
 minus.addEventListener('click', function () {
-    let inputValue = input.value;
-    let resultValue = result.value;
-    result.textContent = subtraction(inputValue, resultValue);
-    return result.value;
+    let inputValue = Number(document.getElementById('input').value);
+    let resultValue = Number(document.getElementById('result').value);
+    result.value = subtraction(inputValue, resultValue);
+    console.log('tell me what my answer is', result.value);
+    result.innerText = result.value;
+    if (result.value >= 0) {
+        result.style.color = 'black';
+    } else if (result.value < 0) {
+        result.style.color = 'red';
+    } else {
+        return null;
+    }
+    return resultValue;
 })
 
