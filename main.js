511dc74fc240bb5514c1b1fc2df21d453b52061a
Sydney Pogue <spogue1441@gmail.com>
2023-04-15 08:45:49 -1000

    completed addNumber and subtractNumber functions, fully functioning

--- main.js
+++ main.js
@@ -1,2 +1,21 @@
 console.log("testing...hello");
 
+const input = document.getElementById("input");
+input.value = 1;
+
+const calculatedNumber = document.getElementById("finalCount");
+
+let count = 0
+
+function addNumber() {
+  count += Number(input.value);
+  calculatedNumber.textContent = count;
+}
+
+function subtractNumber() {
+  count -= Number(input.value);
+  calculatedNumber.textContent = count;
+}
+
+document.getElementById("plusButton").addEventListener("click", addNumber);
+document.getElementById("minusButton").addEventListener("click", subtractNumber);
