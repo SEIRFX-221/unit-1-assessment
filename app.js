8a432392c00343d02b13de3b168a9ca07cb823a4
Alexis Laronga <alexislaronga@Alexiss-MacBook-Air.local>
2023-04-15 14:30:26 -0400

    add: functions and css styling

--- app.js
+++ app.js
@@ -4,3 +4,29 @@ const plus = document.getElementById('plus');
 const input = document.getElementById('input');
 const minus = document.getElementById('minus');
 
+input.value = 1;
+result.textContent = '0';
+
+function addition(num1, num2) {
+    return num1 + num2;
+}
+function subtraction(num1, num2) {
+    return num1 - num2;
+}
+
+plus.addEventListener('click', function () {
+    let inputValue = input.value;
+    let resultValue = result.value;
+    result.textContent = addition(inputValue, resultValue);
+    console.log('this shouldnt be undefined', result.value);
+    return result;
+})
+
+
+minus.addEventListener('click', function () {
+    let inputValue = input.value;
+    let resultValue = result.value;
+    result.textContent = subtraction(inputValue, resultValue);
+    return result.value;
+})
+
