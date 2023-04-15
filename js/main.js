67db6fb81eba00b60bdad64959942ac3928b082f
rufuscodes <rufuscodes@gmail.com>
2023-04-15 12:21:24 -0700

    Progress in javascript

--- js/main.js
+++ js/main.js
@@ -1,26 +1,49 @@
+const counterValue = 0;
+
+ 
+
 // Select body tag from boilerplate
-let body = document.querySelector('body');
+const body = document.querySelector('body');
 console.log(body);
 
 // created html element 'div' to conatin the counter 
-let counterContainer = document.createElement('div');
+const counterContainer = document.createElement('div');
+counterContainer.setAttribute('id', 'counter-container');
 console.log(counterContainer);
 
 // created buttons for the UI
-let addBtn = document.createElement('button');
+const addBtn = document.createElement('button');
 addBtn.textContent = '+';
-let minusBtn = document.createElement('button')
+const minusBtn = document.createElement('button')
 minusBtn.textContent = '-';
 // Test the newly created elements
 console.log(addBtn, minusBtn);
-let digitInput = document.createElement('input');
+const digitInput = document.createElement('input');
 digitInput.placeholder = '0';
 console.log(digitInput);
-
-
-
-let displayText = document.createElement('span');
-displayText.textContent = '0';
-
+const displayText = document.createElement('span');
+displayText.setAttribute('id', 'display-text')
+displayText.textContent = counterValue.valueOf(counterValue);
+// testing the span for displaying digits
 console.log(displayText);
 
+
+body.appendChild(counterContainer);
+counterContainer.appendChild(displayText);
+counterContainer.appendChild(minusBtn);
+counterContainer.appendChild(digitInput);
+counterContainer.appendChild(addBtn);
+
+console.log(body);
+
+
+
+
+addBtn.addEventListener("click", function(){
+
+
+  });
+
+  minusBtn.addEventListener("click", function(){
+
+   });
