4e2b79cb791b32ada27ded906519e2b54b4797dc
Sydney Pogue <spogue1441@gmail.com>
2023-04-15 10:31:31 -1000

    finally fixed the spacing issue with input numbers, added remaining CSS requirements

--- main.js
+++ main.js
@@ -7,6 +7,7 @@ const finalCount = document.getElementById("finalCount");
 
 let count = 0
 
+
 function textColor() {
   finalCount.textContent = count;
   if (count < 0) {
@@ -33,14 +34,20 @@ function subtractNumber() {
 function inputWidth() {
   const value = input.value || "";
   const fontSize = parseFloat(window.getComputedStyle(input).getPropertyValue("font-size"));
+  const canvas = document.createElement("canvas");
+  const ctx = canvas.getContext("2d");
+  ctx.font = `${fontSize}px sans-serif`;
+  const textWidth = ctx.measureText(value).width;
   const padding = 5;
-  const adjustedWidth = value.length * fontSize + padding;
+  const adjustedWidth = textWidth + padding * 2 + 10;
   input.style.width = `${adjustedWidth}px`;
 }
 
+
 input.addEventListener("input", inputWidth);
+window.addEventListener("resize", inputWidth);
 
-
+inputWidth();
 
 document.getElementById("plusButton").addEventListener("click", addNumber);
 document.getElementById("minusButton").addEventListener("click", subtractNumber);
