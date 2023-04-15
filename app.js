7f9221d5b2bf78768449def8f46ece1222686889
badoodee95 <monzoncaleb@yahoo.com>
2023-04-15 11:55:31 -0700

    Finished javascript with minimal css done

---
+++ app.js
@@ -0,0 +1,59 @@
+// DECLARE VARIABLES AND GRAB HTML ELEMENTS
+let countDisplay = document.querySelector('.count-display');
+let buttonPlus = document.querySelector('.plus');
+let buttonMinus = document.querySelector('.minus');
+let input = document.querySelector('.input');
+let count = 0;
+
+
+
+function buttonPlusHit() {
+    countDisplay = document.querySelector('.count-display');
+    let inputValue = input.value;
+    if (countDisplay.textContent === '0') {
+        count = parseInt(inputValue);
+        countDisplay.textContent = 0 + count;
+        countDisplay = document.querySelector('.count-display');
+        colorBluePositive();
+    } else if (countDisplay.textContent !== '0' ){
+        count = parseInt(inputValue);
+        countDisplay.textContent = parseInt(countDisplay.textContent) + count;
+        countDisplay = document.querySelector('.count-display');
+        colorBluePositive();
+    } 
+
+}
+buttonPlus.addEventListener('click', buttonPlusHit);
+
+function buttonMinusHit() {
+    countDisplay = document.querySelector('.count-display');
+    let inputValue = input.value;
+    if (countDisplay.textContent === '0') {
+        count = parseInt(inputValue);
+        countDisplay.textContent = 0 - count;
+        countDisplay = document.querySelector('.count-display');
+        colorRedNegative();
+    } else if (countDisplay.textContent !== '0') {
+        countDisplay = document.querySelector('.count-display');
+        count = parseInt(inputValue);
+        countDisplay.textContent -= count;
+        countDisplay = document.querySelector('.count-display');
+        colorRedNegative();
+    }
+}
+
+buttonMinus.addEventListener('click', buttonMinusHit);
+
+function colorRedNegative() {
+    countDisplay = document.querySelector('.count-display');
+    if (parseInt(countDisplay.textContent) < 0) {
+        countDisplay.style.color = 'red';
+    }
+}
+
+function colorBluePositive() {
+    countDisplay = document.querySelector('.count-display');
+    if (parseInt(countDisplay.textContent) > 0) {
+        countDisplay.style.color = 'blue';
+    }
+}
