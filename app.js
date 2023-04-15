5ee5c4e958a94becedd73b0dbce0d37e0a278ac4
Alexis Laronga <alexislaronga@Alexiss-MacBook-Air.local>
2023-04-15 13:59:23 -0400

    add: html elements and global variables in js

---
+++ app.js
@@ -0,0 +1,6 @@
+const math = document.getElementById('math');
+const result = document.getElementById('result');
+const plus = document.getElementById('plus');
+const input = document.getElementById('input');
+const minus = document.getElementById('minus');
+
