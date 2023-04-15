98ab53063fed1ac5efdfb68762c68569dbd66f11
Sydney Pogue <spogue1441@gmail.com>
2023-04-15 08:59:33 -1000

    challenge added: display turns red if number is negative

--- main.js
+++ main.js
@@ -3,21 +3,34 @@ console.log("testing...hello");
 const input = document.getElementById("input");
 input.value = 1;
 
-const calculatedNumber = document.getElementById("finalCount");
+const finalCount = document.getElementById("finalCount");
 
 let count = 0
 
+function textColor() {
+  finalCount.textContent = count;
+  if (count < 0) {
+    finalCount.style.color = "red";
+  } else {
+    finalCount.style.color = "black";
+  }
+}
+
 function addNumber() {
   count += Number(input.value);
-  calculatedNumber.textContent = count;
+  finalCount.textContent = count;
   input.value = "";
+  textColor();
 }
 
 function subtractNumber() {
   count -= Number(input.value);
-  calculatedNumber.textContent = count;
+  finalCount.textContent = count;
   input.value = "";
+  textColor();
 }
+
+
 
 document.getElementById("plusButton").addEventListener("click", addNumber);
 document.getElementById("minusButton").addEventListener("click", subtractNumber);
